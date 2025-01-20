6a273d006cb20e5ec5b1d4939ce5b849070e8a2b
DeboraSantos <deboramargaretedossantos@gmail.com>
2025-01-19 22:31:05 -0300

    demonstracao_resultado_acao_refente_duracao_contrato

--- inicial.py
+++ inicial.py
@@ -37,13 +37,23 @@ for coluna in tabela.columns:
 
 # Análise dos gráficos e ações para resolução das possíveis causas de cancelamento do serviço:
 
-# Causa: Usuários de contrato mensal sempre cancelam.
-# Ação corretiva: evitar o contrato mensal e incentivar (com desconto) os contratos trimestrais e anuais.
+# 1ªCausa: Usuários de contrato mensal sempre cancelam.
+ª
+# 2ªCausa: Todos os usuários que ligam mais de 4x para o call center, cancelaram o serviço.
+# 2ªAção: Criar um alerta vermelho, que acuse quando o usuário já realizou a terceira ligação para o call center.
 
-# Causa: Todos os usuários que ligam mais de 4x para o call center, cancelaram o serviço.
-# Ação: Criar um alerta vermelho, que acuse quando o usuário já realizou a terceira ligação para o call center.
-
-# Causa: Usuários que atrasaram o pagamento por mais de 20 dias, cancelaram o serviço.
-# Ação: Criar alerta para quando o usuário atingiu 15 dias de atraso no pagamento.
+# 3ªCausa: Usuários que atrasaram o pagamento por mais de 20 dias, cancelaram o serviço.
+# 3ªAção: Criar alerta para quando o usuário atingiu 15 dias de atraso no pagamento.
 
 # Com essas três ações estratégicas é possível reduzir consideravelmente o percentual de cancelamento:
+
+# duracao_contrato:
+ 
+tabela = tabela[tabela["duracao_contrato"]!="Monthly"]
+
+# atraso_pagamento:
+
+print(tabela["cancelou"].value_counts)
+
+# percentual:
+print(tabela["cancelou"].value_counts(normalize=True))
